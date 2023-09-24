eddef239421c97d88318d2221fadabe9ebb5f301
TheJuloX <julian.castro01@usa.edu.co>
2023-09-24 18:44:51 -0500

    PREVIOS MONTH Y NEXT

--- cal/utils.py
+++ cal/utils.py
@@ -22,7 +22,6 @@ class Calendar(HTMLCalendar):
 		d = ''
 		for event in events_per_day:
 			d += f'<li> {event.title} </li>'
-				
 
 		if day != 0:
 			return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
