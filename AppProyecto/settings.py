14d1b47674e6aa620852e6d5e3c956a4821c6b37
TheJuloX <julian.castro01@usa.edu.co>
2023-09-24 12:35:42 -0500

    MIGRACIONES DE CALENDARIO A BASE

--- AppProyecto/settings.py
+++ AppProyecto/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainPage'
+    'mainPage',
+    'schedule',
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
