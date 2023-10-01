bbc644fa1d0ad180a2a54f4933bfff3358aa6aea
TheJuloX <julian.castro01@usa.edu.co>
2023-10-01 14:11:07 -0500

    Calendario actualizacion

--- cal/views.py
+++ cal/views.py
@@ -17,23 +17,17 @@ class CalendarView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # use today's date for the calendar
-        d = get_date(self.request.GET.get('day', None))
-        context['prev_month'] = prev_month(d)
-        context['next_month'] = next_month(d)
-        # Instantiate our calendar class with today's year and date
+        d = get_date(self.request.GET.get('month', None))
         cal = Calendar(d.year, d.month)
-
-        # Call the formatmonth method, which returns our calendar as a table
         html_cal = cal.formatmonth(withyear=True)
         context['calendar'] = mark_safe(html_cal)
-
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
         return context
 
-def get_date(req_day):
-    if req_day:
-        year, month = (int(x) for x in req_day.split('-'))
+def get_date(req_month):
+    if req_month:
+        year, month = (int(x) for x in req_month.split('-'))
         return date(year, month, day=1)
     return datetime.today()
 
@@ -49,3 +43,4 @@ def next_month(d):
     next_month = last + timedelta(days=1)
     month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
     return month
+
