83be73fac6a94948b4d3aa05d5ebc29ed295072a
TheJuloX <julian.castro01@usa.edu.co>
2023-10-01 18:30:00 -0500

    FORO 1.0

--- mainPage/models.py
+++ mainPage/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 # Create your models here.
