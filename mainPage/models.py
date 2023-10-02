e664d52b24f0ec3fc4bf75fe72696727df281d92
TheJuloX <julian.castro01@usa.edu.co>
2023-10-01 19:22:00 -0500

    ERROR PUTIADA

--- mainPage/models.py
+++ mainPage/models.py
@@ -1,9 +1,4 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
 # Create your models here.
