08f5b070f2e7db627d0f8812d5dd01e370cb3246
DavidAvila6 <david.avila02sa.edu.co>
2023-10-15 20:58:01 -0500

    Prototipo de vista cambio

--- mainPage/static/js/perfil.js
+++ mainPage/static/js/perfil.js
@@ -1,4 +1,23 @@
-
-    document.getElementById('cambiarImagen').addEventListener('click', function() {
-        document.body.classList.toggle('cambiado');
+// perfil.js
+document.addEventListener("DOMContentLoaded", function () {
+    var body = document.body;
+  
+    // Verifica si hay un estado almacenado en localStorage
+    var isChanged = localStorage.getItem("isChanged") === "true";
+  
+    // Aplica la clase "cambiado" si es necesario
+    if (isChanged) {
+      body.classList.add("cambiado");
+    }
+  
+    // Agrega un evento al botón para cambiar el fondo
+    document.getElementById("cambiarImagen").addEventListener("click", function () {
+      // Cambia el estado y la clase del cuerpo
+      isChanged = !isChanged;
+      body.classList.toggle("cambiado", isChanged);
+  
+      // Almacena el estado en localStorage
+      localStorage.setItem("isChanged", isChanged.toString());
     });
+  });
+  
