48ccff507b24017bcf9dcc249e7701e00d28514b
DavidAvila6 <david.avila02@usa.edu.co>
2023-10-19 19:09:57 -0500

    ds

--- mainPage/forms.py
+++ mainPage/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from  models import Comentario
 
 from mainPage.models import Beca, Publicacion, Pregunta, ElegirRespuesta, PreguntasRespondidas
 
