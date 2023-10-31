30b382666195b543f4e17679db15ad0fbc8e820f
Santiago01 <jose.bautista01@usa.edu.co>
2023-10-30 19:16:52 -0500

    modal con navegacion

--- mainPage/static/js/hub.js
+++ mainPage/static/js/hub.js
@@ -1,3 +1,23 @@
 window.addEventListener('load', function() {
     document.getElementById('container-modal').style.display = 'block';
 });
+
+
+function mostrarSiguiente(actual, siguiente) {
+    var modalActual = document.getElementById(actual);
+    var modalSiguiente = document.getElementById(siguiente);
+
+    modalActual.style.display = "none";
+    modalSiguiente.style.display = "block";
+}
+
+function mostrarAnterior(actual, anterior) {
+    var modalActual = document.getElementById(actual);
+    var modalAnterior = document.getElementById(anterior);
+
+    modalActual.style.display = "none";
+    modalAnterior.style.display = "block";
+}
+
+
+
