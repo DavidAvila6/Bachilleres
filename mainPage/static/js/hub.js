450a74c41aeaa96ca1870c839d94f412bb6aac3d
Santiago01 <jose.bautista01@usa.edu.co>
2023-11-01 20:37:57 -0500

    modales terminados pero no del todo

--- mainPage/static/js/hub.js
+++ mainPage/static/js/hub.js
@@ -1,23 +1,34 @@
 window.addEventListener('load', function() {
-    document.getElementById('container-modal').style.display = 'block';
+    var modal = document.getElementById('container-modal');
+    var btnModal = document.getElementById('btn-modal');
+
+    // Verificar si la cookie 'modalMostrado' está presente
+    var modalMostrado = localStorage.getItem('modalMostrado');
+
+    if (!modalMostrado) {
+        // Si la cookie no está presente, mostrar el modal
+        modal.style.display = 'block';
+    }
+
+    // Asignar un evento al botón de cerrar
+    document.getElementById('btn-cerrar').addEventListener('click', function() {
+        modal.style.display = 'none';
+        
+        // Al cerrar el modal, establecer la cookie 'modalMostrado' para evitar que se muestre nuevamente
+        localStorage.setItem('modalMostrado', 'true');
+    });
+
+    // Asignar un evento al botón de siguiente
+    document.getElementById('btn-siguiente').addEventListener('click', function() {
+        window.location.href = 'recursos';
+    });
 });
 
+/* window.addEventListener('load', function() {
+    document.getElementById('container-modal').style.display = 'block';
+});*/
 
-function mostrarSiguiente(actual, siguiente) {
-    var modalActual = document.getElementById(actual);
-    var modalSiguiente = document.getElementById(siguiente);
 
-    modalActual.style.display = "none";
-    modalSiguiente.style.display = "block";
-}
-
-function mostrarAnterior(actual, anterior) {
-    var modalActual = document.getElementById(actual);
-    var modalAnterior = document.getElementById(anterior);
-
-    modalActual.style.display = "none";
-    modalAnterior.style.display = "block";
-}
 
 
 
