555ece0824b4ba34b5f0af774f9aca638710b493
Santiago01 <jose.bautista01@usa.edu.co>
2023-11-12 13:51:00 -0500

    arreglo cookies

--- mainPage/static/js/hub.js
+++ mainPage/static/js/hub.js
@@ -1,16 +1,6 @@
-window.addEventListener('load', function() {
-    // Verificar si el modal ya ha sido mostrado
-    if (!localStorage.getItem('modalMostrado')) {
-        // Si no ha sido mostrado, mostrar el modal
-        document.getElementById('container-modal').style.display = 'block';
-        // Marcar el modal como mostrado en el almacenamiento local
-        localStorage.setItem('modalMostrado', 'true');
-    }
-})
 
-/* window.addEventListener('load', function() {
-    document.getElementById('container-modal').style.display = 'block';
-});*/
+
+
 
 
 
